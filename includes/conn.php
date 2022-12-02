af1bd273769532521db5e8eb06dc21b440309312
NaoSou0Veloso <86413183+NaoSou0Veloso@users.noreply.github.com>
2022-12-02 13:55:04 -0300

    Add files via upload

---
+++ includes/conn.php
@@ -0,0 +1,13 @@
+<?php
+$db_host='localhost';
+$db_usuario='root';
+$db_senha='';
+$db_base='acervo';
+
+$conn = mysqli_connect($db_host, $db_usuario, $db_senha, $db_base);
+
+if (!$conn){
+    die("Connection failed:" . mysqli_connect_error());
+}
+
+?>
